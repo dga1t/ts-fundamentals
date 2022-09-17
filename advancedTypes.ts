20fd3a4c2a15e9c29a6f8e0bf3d1c8902c5d13d2
Dima Pasumansky <dpsmnsk@gmail.com>
2022-09-17 19:43:31 +0500

    lession 22

--- advancedTypes.ts
+++ advancedTypes.ts
@@ -11,17 +11,44 @@ function logError(err: string | string[]) {
 }
 
 function logObject(obj: { a: number } | { b: number }) {
-    if ('a' in obj) console.log(obj.a)
-    else console.log(obj.b)
+  if ('a' in obj) console.log(obj.a);
+  else console.log(obj.b);
 }
 
 // =====================================================
 // vid #21 Literal Types
 
 function fetchWithAuth(url: string, method: 'post' | 'get'): 1 | -1 {
-    return 1
+  return 1;
 }
 
 // =====================================================
 // vid #22 Type Aliases
 
+type httpMethod = 'post' | 'get';
+
+function fetchWithAuth2(url: string, method: httpMethod): 1 | -1 {
+  return 1;
+}
+
+type User = {
+  name: string;
+  age: number;
+  skills: string[];
+};
+
+type Role = {
+  id: number;
+};
+
+type UserWithRole = User & Role;
+
+let user: UserWithRole = {
+  name: 'asd',
+  age: 33,
+  skills: ['1', '2'],
+  id: 1,
+};
+
+// =====================================================
+// vid #23 Interfaces
