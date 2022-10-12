653fe7fd142fb785d9dff1348fed7e29c66c43f5
Dima Pasumansky <dpsmnsk@gmail.com>
2022-10-12 21:06:47 +0500

    vids 35-37

---
+++ classes.ts
@@ -0,0 +1,14 @@
+// =====================================================
+// vid #35 Create class
+
+// in tsconfig - strictPropertyInitialization is often set to false, so that class can be used as interface
+// especially together with decorators
+
+// =====================================================
+// vid #36 Constructor
+
+// in TS constructor can be overloaded
+
+// =====================================================
+// vid #37 Methods
+
