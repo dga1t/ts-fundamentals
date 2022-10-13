d7d86cef6da4370e111e4adcf4f57c6e3a4703a9
Dima Pasumansky <dpsmnsk@gmail.com>
2022-10-13 21:33:17 +0500

    vids 38-40

--- classes.ts
+++ classes.ts
@@ -12,3 +12,42 @@
 // =====================================================
 // vid #37 Methods
 
+// =====================================================
+// vid #38 Exercise - Method overload
+
+// wtf would anyone write this?!
+class UserOne {
+  skills: string[];
+
+  addSkill(skill: string): void;
+  addSkill(skills: string[]): void;
+  addSkill(skillOrSkills: string | string[]): void {
+    if (typeof skillOrSkills == 'string') this.skills.push(skillOrSkills);
+    else this.skills.push(...skillOrSkills);
+  }
+}
+
+// =====================================================
+// vid #39 Getters and Setters
+
+// getters and setters can only be synchronous
+
+// =====================================================
+// vid #40 Implements
+
+interface ILogger {
+  log(...args): void;
+  error(...args): void;
+}
+
+class Logger implements ILogger {
+  log(...args: any[]): void {
+    console.log(...args);
+  }
+  async error(...args: any[]): Promise<void> {
+    console.log(...args);
+  }
+}
+
+// =====================================================
+// vid #41 Extends
