4df0b5f165c0d18216c8b2d5898e124cc2b1cb02
Dima Pasumansky <dpsmnsk@gmail.com>
2022-10-22 21:01:35 +0500

    vids 64-66

--- generics.ts
+++ generics.ts
@@ -103,3 +103,92 @@ function logId<T extends string | number, Y>(
 
 // =====================================================
 // vid #64 Exercise
+
+interface ID {
+  id: number;
+}
+
+function sort<T extends ID>(data: T[], type: 'asc' | 'desc' = 'asc'): T[] {
+  return data.sort((a, b) => {
+    switch (type) {
+      case 'asc':
+        return a.id - b.id;
+      case 'desc':
+        return b.id - a.id;
+    }
+  });
+}
+
+// =====================================================
+// vid #65 Generic classes
+
+class Resp<D, E> {
+  data?: D;
+  error?: E;
+
+  constructor(data?: D, error?: E) {
+    this.data = data;
+    this.error = error;
+  }
+}
+
+const res = new Resp<string, number>('data', 0);
+
+class HTTPResp<F> extends Resp<string, number> {
+  code: F;
+
+  setCode(code: F) {
+    this.code = code;
+  }
+}
+
+const res2 = new HTTPResp();
+
+// =====================================================
+// vid #66 Mixins
+
+// basic & generis constructors
+type Constructor = new (...args: any[]) => {};
+type GConstructor<T = {}> = new (...args: any[]) => T;
+
+class List {
+  constructor(public items: string[]) {}
+}
+
+class Accordeon {
+  isOpened: boolean;
+}
+
+type ListType = GConstructor<List>;
+type AccordeonType = GConstructor<Accordeon>;
+
+// we could achieve mixin functionality with class like this
+class ExtendedListClass extends List {
+  first() {
+    return this.items[0];
+  }
+}
+
+// and the actual mixin:
+// we pass a class to a mixin function that returns another class
+// that extends class that was passed in as an argument
+function ExtendedList<TBase extends ListType & AccordeonType>(Base: TBase) {
+  return class ExtendedList extends Base {
+    first() {
+      return this.items[0];
+    }
+  };
+}
+
+class AccordeonList {
+  isOpened: boolean;
+  constructor(public items: string[]) {}
+}
+
+const list = ExtendedList(AccordeonList);
+const result = new list(['first', 'second']);
+console.log(result.first());
+
+// so a mixin allows us extend from multiple classes at the same time with typechecking
+// mixins are widely used in DCI (data, context and interaction) paradigm
+// and are usefull when you are building complex structures from small parts
