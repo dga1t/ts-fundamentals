9c3bdf4a7397cbbc4380fbd1835c85d071713a0c
Dima Pasumansky <dpsmnsk@gmail.com>
2022-10-29 23:18:21 +0500

    vid 69

--- manipulationsWithTypes.ts
+++ manipulationsWithTypes.ts
@@ -21,3 +21,34 @@ function getValue<T, K extends keyof T>(obj: T, key: K) {
 
 // =====================================================
 // vid #69 Exercise
+
+interface Data {
+  group: number;
+  name: string;
+}
+
+interface IGroup<T> {
+  [key: string]: T[];
+}
+
+type key = string | number | symbol;
+
+// so we need to group by some property that necessarily exists in the objects that we group
+function group<T extends Record<key, any>>(
+  array: T[],
+  key: keyof T
+): IGroup<T> {
+  return array.reduce<IGroup<T>>((map: IGroup<T>, item) => {
+    const itemKey = item[key];
+    let curEl = map[itemKey];
+
+    if (Array.isArray(curEl)) curEl.push(item);
+    else curEl = [item];
+    map[itemKey] = curEl;
+    return map;
+  }, {});
+}
+
+// =====================================================
+// vid #70 Typeof
+
