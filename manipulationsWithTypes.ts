a852568327bdd94a75a8a257698054174bf051df
Dima Pasumansky <dpsmnsk@gmail.com>
2022-10-30 17:33:48 +0500

    vids 70-71

--- manipulationsWithTypes.ts
+++ manipulationsWithTypes.ts
@@ -52,3 +52,49 @@ function group<T extends Record<key, any>>(
 // =====================================================
 // vid #70 Typeof
 
+let strOrNum: string | number;
+
+if (Math.random() > 0.5) strOrNum = 5;
+else strOrNum = 'str';
+
+let strOrNum2: typeof strOrNum;
+
+// keyof typeof example 1
+const userTwo = { name: 'vasya' };
+type keyOfUser = keyof typeof userTwo;
+
+// keyof typeof example 2
+enum Direction {
+  Up,
+  Down,
+}
+type d = keyof typeof Direction;
+
+// so typeof also allows us to get the type of an object and use it
+
+// =====================================================
+// vid #71 Indexed Access Types
+
+interface Role2 {
+  name: string;
+}
+
+interface Permission {
+  endDate: Date;
+}
+
+interface User2 {
+  name: string;
+  roles: Role2[];
+  permission: Permission;
+}
+
+type rolesType = User2['roles'];
+type roleType = User2['roles'][number];
+type dateType = User2['permission']['endDate']
+
+// this is a usefull hack if you want to 'convert' some runtime strings
+// to types that you can use later on
+const roles = ['admin', 'user', 'superuser'] as const;
+type roleTypes = typeof roles[number];
+
