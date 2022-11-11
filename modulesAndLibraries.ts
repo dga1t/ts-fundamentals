808d619217e1b33d92f13c385d5b9de521fc11a9
Dima Pasumansky <dpsmnsk@gmail.com>
2022-11-11 22:46:22 +0500

    vids 94-95

---
+++ modulesAndLibraries.ts
@@ -0,0 +1,19 @@
+// =====================================================
+// vid #94 Intro
+
+// =====================================================
+// vid #95 Namespaces and reference
+
+// namespaces incapsulate whatever and sometimes are usefull
+namespace A {
+  export const a = 5;
+  export interface B {
+    c: number;
+  }
+}
+
+// reference allows to bundle everything into one file and should not be used at all :E
+
+// =====================================================
+// vid #96 Modules on backend
+
