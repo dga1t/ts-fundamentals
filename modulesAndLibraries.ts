5e2855c0b4aab47115a002cfdd27a80639832a18
Dima Pasumansky <dpsmnsk@gmail.com>
2022-11-13 21:05:49 +0500

    vids 97-98

--- modulesAndLibraries.ts
+++ modulesAndLibraries.ts
@@ -17,3 +17,19 @@ namespace A {
 // =====================================================
 // vid #96 Modules on backend
 
+// =====================================================
+// vid #97 Modules on frontend
+
+// =====================================================
+// vid #98 Import and export
+
+// legal import examples 
+
+// import run, { a, type MyType } from './module/app';
+// import running from './module/app';
+// import * as all from './module/app';
+// import { Test as TestName } from './module/app';
+// import { type MyTypeTwo } from './module/app';
+
+// =====================================================
+// vid #99 Third party libs types
