9e96ac36a574abd9f1529289706a41be6c18fb67
Dima Pasumansky <dpsmnsk@gmail.com>
2022-11-20 18:19:18 +0500

    vids 114-115

--- behavioralPatterns.ts
+++ behavioralPatterns.ts
@@ -115,3 +115,156 @@ const notifications = new Notifications();
 const m = new NotificationMediator(notifications, logger, handler);
 handler.setMediator(m);
 handler.myEvent();
+
+// =====================================================
+// vid #114 Command
+
+// on the frontend its is often used in state management
+// on the backend in cqrs pattern (command-query responsibility segregation)
+
+class UserSeven {
+  constructor(public userId: number) {}
+}
+
+class CommandHistory {
+  public commands: Command[] = [];
+  push(command: Command) {
+    this.commands.push(command);
+  }
+  remove(command: Command) {
+    this.commands = this.commands.filter(
+      (c) => c.commandId !== command.commandId
+    );
+  }
+}
+
+// abstract command can have various logic in it
+abstract class Command {
+  public commandId: number;
+
+  abstract execute(): void;
+
+  constructor(public history: CommandHistory) {
+    this.commandId = Math.random();
+  }
+}
+
+// the concrete actual realization of command
+class AddUserCommand extends Command {
+  constructor(
+    private user: UserSeven,
+    private receiver: UserServiceTwo,
+    history: CommandHistory
+  ) {
+    super(history);
+  }
+
+  execute(): void {
+    this.receiver.saveUser(this.user);
+    this.history.push(this);
+  }
+
+  undo() {
+    this.receiver.deleteUser(this.user.userId);
+    this.history.remove(this);
+  }
+}
+
+class UserServiceTwo {
+  saveUser(user: UserSeven) {}
+  deleteUser(userId: number) {}
+}
+
+class ControllerTwo {
+  receiver: UserServiceTwo;
+  history: CommandHistory = new CommandHistory();
+
+  addReceiver(receiver: UserServiceTwo) {
+    this.receiver = receiver;
+  }
+
+  run() {
+    const addUserCommand = new AddUserCommand(
+      new UserSeven(1),
+      this.receiver,
+      this.history
+    );
+    addUserCommand.execute();
+    addUserCommand.undo();
+  }
+}
+
+const controllerTwo = new ControllerTwo();
+controllerTwo.addReceiver(new UserServiceTwo());
+controllerTwo.run();
+
+// =====================================================
+// vid #115 State
+
+class DocumentItem {
+  public text: string;
+  private state: DocumentItemState;
+
+  constructor() {
+    this.setState(new DraftDocumentItemState());
+  }
+  getState() {
+    return this.state;
+  }
+  setState(state: DocumentItemState) {
+    this.state = state;
+    this.state.setContext(this);
+  }
+  publishDoc() {
+    this.state.publish();
+  }
+  deleteDoc() {
+    this.state.delete();
+  }
+}
+
+abstract class DocumentItemState {
+  public name: string;
+  public item: DocumentItem;
+
+  public setContext(item: DocumentItem) {
+    this.item = item;
+  }
+
+  public abstract publish(): void;
+  public abstract delete(): void;
+}
+
+class DraftDocumentItemState extends DocumentItemState {
+  constructor() {
+    super();
+    this.name = 'DraftDocument';
+  }
+  public publish(): void {
+    this.item.setState(new PublishDocumentItemState());
+  }
+  public delete(): void {
+    console.log('deleted');
+  }
+}
+class PublishDocumentItemState extends DocumentItemState {
+  constructor() {
+    super();
+    this.name = 'PublishDocument';
+  }
+  public publish(): void {
+    console.log('cant publish doc again');
+  }
+  public delete(): void {
+    this.item.setState(new DraftDocumentItemState());
+  }
+}
+
+const item = new DocumentItem();
+item.text = 'som text';
+console.log(item.getState());
+item.publishDoc();
+item.deleteDoc();
+
+// =====================================================
+// vid #116 Strategy
